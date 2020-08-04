c76be746b4034bcafdeaaeb56dd991e36dfb84e9
Joshua <joshua.richardson@myunidays.com>
2020-08-04 18:10:29 +0100

    Added logged in user list

--- lib/Pages/ThoughtsAndCrossesPage.dart
+++ lib/Pages/ThoughtsAndCrossesPage.dart
@@ -3,21 +3,15 @@ import 'package:signalr_core/signalr_core.dart';
 
 import 'Dialog.dart';
 
-
-//final serverUrl = 'games-by-joshua.herokuapp.com/chatHub';
+//final serverUrl = 'https://games-by-joshua.herokuapp.com/chatHub';
 final serverUrl = 'http://192.168.1.76:5001/chatHub';
-final connection = HubConnectionBuilder().withUrl(serverUrl,
-    HttpConnectionOptions(
-      logging: (level, message) => print(message),
-    )).build();
-
-Future _connectToHub() async {
-  connection.onclose((error) => print("Connection closed"));
-  await connection.start();
-  print('We got here');
-  connection.on('LoggedInUsers', (message) => print(message.toString()));
-  connection.on('ReceiveLetter', (message) => print(message.toString()));
-}
+final connection = HubConnectionBuilder()
+    .withUrl(
+        serverUrl,
+        HttpConnectionOptions(
+          logging: (level, message) => print(message),
+        ))
+    .build();
 
 class ThoughtsAndCrossesPage extends StatefulWidget {
   ThoughtsAndCrossesPage({Key key}) : super(key: key);
@@ -26,6 +20,43 @@ class ThoughtsAndCrossesPage extends StatefulWidget {
 }
 
 class _ThoughtsAndCrossesPageState extends State<ThoughtsAndCrossesPage> {
+  final _loggedInUserList = <Widget>[];
+  Future _connectToHub() async {
+    connection.onclose((error) => print("Connection closed"));
+    await connection.start();
+    connection.on('LoggedInUsers', (message) => renderStuff(message[0]));
+    connection.on('ReceiveLetter', (message) => print(message.toString()));
+  }
+
+  renderStuff(List users) {
+    _loggedInUserList.clear();
+
+    setState(() {
+      users.forEach((user) => _loggedInUserList.add(ListTile(
+        title: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              color: Colors.deepPurple,
+            ),
+            child: SizedBox(
+              height: 48,
+              width: 200,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Center(
+                  child: Text(user,
+                  style: TextStyle(
+                    color: Colors.white
+                  )),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )));
+    });
+  }
 
   @override
   void initState() {
@@ -35,24 +66,48 @@ class _ThoughtsAndCrossesPageState extends State<ThoughtsAndCrossesPage> {
     super.initState();
   }
 
+  Widget _buildListItems() {
+    final _users = <Widget>[];
+    _loggedInUserList.forEach((user) => _users.add(user));
+
+    return ListView(
+      shrinkWrap: true,
+      children: _users,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-        return Center(
-      child: RaisedButton(
-        onPressed: () async {
-          final name = await showDialog(context: context, builder: (BuildContext context) => loginDialog);
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () async {
+              final name = await showDialog(
+                  context: context,
+                  builder: (BuildContext context) => loginDialog);
 
-          await connection.invoke("AddToGroup", args: ['GroupOfJoshua']);
-          await connection.invoke('JoinRoom', args: ['GroupOfJoshua', name, 0]);
-        },
-        color: Colors.deepPurple,
-        child: Text(
-          'Login',
-          style: TextStyle(
-            color: Colors.white,
+              await connection.invoke("AddToGroup", args: ['GroupOfJoshua']);
+              await connection.invoke("Startup", args: ['GroupOfJoshua', name, 0]);
+              await connection.invoke("SetupNewUser", args: ['GroupOfJoshua', name]);
+              await connection.invoke("AddToGroup", args: ['GroupOfJoshua']);
+              await connection.invoke('JoinRoom', args: ['GroupOfJoshua', name, 0]);
+            },
+            color: Colors.deepPurple,
+            child: Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
           ),
-        ),
+          Expanded(
+            child: _buildListItems(),
+          ),
+        ],
       ),
     );
   }
 }
+
