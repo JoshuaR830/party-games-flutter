94efccf24e7a22a0997c850d1acbbd49ccf66f2e
Joshua <psyjr4@nottingham.ac.uk>
2020-08-11 07:53:54 +0100

    Added splash scrren and connection error page on startup with retry button

---
+++ lib/Pages/ConnectionError.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class ConnectionErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Connection Error',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Scaffold(
+        backgroundColor: Color(0xFF8f92c9),
+        appBar: AppBar(
+          title: Text("Connection Error"),
+        ),
+        body: Column(
+        children: <Widget>[
+          Center(
+            child: Text("A connection error occurred"),
+          ),
+          RaisedButton(
+            child: Text("Retry"),
+            onPressed: () async {
+              setupConnection();
+            },
+            color: Colors.deepPurple,
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:party_games/Pages/GamesLibraryPage.dart';
 import 'package:signalr_core/signalr_core.dart';
+import 'Pages/ConnectionError.dart';
 import 'Pages/ThoughtsAndCrossesPage.dart';
 
 int countDown = 0;
 
 Future<void> main() async {
-  connection.onclose((error) => print("Connection closed"));
-  await connection.start();
-  runApp(PartyGames());
+  setupConnection();
 }
 
+Future<void> setupConnection() async {
+  connection.onclose((error) => print("Connection closed"));
+  try {
+    await connection.start();
+    runApp(PartyGames());
+  } catch (e){
+    print("There was a problem connecting to the server");
+    runApp(ConnectionError());
+  }
+}
 
 final serverUrl = 'https://games-by-joshua.herokuapp.com/chatHub';
-//final serverUrl = 'http://192.168.1.76:5001/chatHub';
+//final serverUrl = 'http://192.168.1.86:5001/chatHub';
 //final serverUrl = 'http://192.168.1.72:5001/chatHub';
 final connection = HubConnectionBuilder()
     .withUrl(
@@ -28,5 +37,13 @@ class PartyGames extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GamesLibraryPage();
+  }
+}
+
+class ConnectionError extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ConnectionErrorPage();
   }
 }
